6335a4cdca1e5c80eb18fb833752f2fce80dc151
ismailtemel <64991440+ismailtemel@users.noreply.github.com>
2024-10-04 15:02:05 +0300

    New Commit

---
+++ BurgerProjesi/burger/src/components/About.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import BannerImage from '../assets/banneryeni.jpg'
+import '../styles/About.css'
+
+export const About = () => {
+  return (
+    <div className='about'>
+        <div className='aboutTop' style={{backgroundImage:`url(${BannerImage})`}}></div>
+        <div className='aboutBottom'>
+          <h1>Hakkımızda</h1>
+          <p>Dolore laborum officia ut non exercitation qui consequat laborum anim. Fugiat mollit officia 
+            occaecat aliqua. Ullamco proident sint amet aliqua enim laboris sint ad deserunt aute excepteur.
+            Dolore laborum officia ut non exercitation qui consequat laborum anim. Fugiat mollit officia 
+            occaecat aliqua. Ullamco proident sint amet aliqua enim laboris sint ad deserunt aute excepteur.
+            Dolore laborum officia ut non exercitation qui consequat laborum anim. Fugiat mollit officia 
+            occaecat aliqua. Ullamco proident sint amet aliqua enim laboris sint ad deserunt aute excepteur.
+            Dolore laborum officia ut non exercitation qui consequat laborum anim. Fugiat mollit officia 
+            occaecat aliqua. Ullamco proident sint amet aliqua enim laboris sint ad deserunt aute excepteur.
+            Dolore laborum officia ut non exercitation qui consequat laborum anim. Fugiat mollit officia 
+            occaecat aliqua. Ullamco proident sint amet aliqua enim laboris sint ad deserunt aute excepteur.
+            Dolore laborum officia ut non exercitation qui consequat laborum anim. Fugiat mollit officia 
+            occaecat aliqua. Ullamco proident sint amet aliqua enim laboris sint ad deserunt aute excepteur.
+            Dolore laborum officia ut non exercitation qui consequat laborum anim. Fugiat mollit officia 
+            occaecat aliqua. Ullamco proident sint amet aliqua enim laboris sint ad deserunt aute excepteur.
+          </p>
+        </div>
+    </div>
+  )
+}
